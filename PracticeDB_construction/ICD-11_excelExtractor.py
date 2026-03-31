5ebcf450f14c5093ceffcd46c298d392d12db886
velma the vivobook <u21551228@tuks.co.za>
2026-03-31 12:23:18 +0200

    Made a new csv (ICD-11NoPointLevel.csv) that has the same entries as ICD-11_6_8 except it excludes the lower level classificatios where things have a point ie entries like 6A00.2. This is because it seems unecessary. There is an issue because the heirarchy levels are inconsistent -> see mood disorders are second level and have no code while schizophrenia is second level and does have a code

--- PracticeDB_construction/ICD-11_excelExtractor.py
+++ PracticeDB_construction/ICD-11_excelExtractor.py
@@ -1,12 +1,23 @@
 import numpy
 import pandas as pd
 
-# //////////////////////////////////////////////////////////////////////////////////make the ICD-11 codes neurological only
-path = "PracticeDB_construction/SimpleTabulation-ICD-11-MMS-en.xlsx"
+# ///extracting the ICD-11 codes for neurological and psycological disorders to ICD-11_6_8.csv, which will be used to create the PracticeDB for neurological and psychological disorders
+"""path = "PracticeDB_construction/SimpleTabulation-ICD-11-MMS-en.xlsx"
 
 df = pd.read_excel(path)
 dfPsychoological = df.loc[3565:4493]
 dfNeurological = df.loc[4580:5468]
 
 dfBoth = pd.concat([dfPsychoological, dfNeurological], ignore_index=True)
-dfBoth.to_csv("/home/school/masters/Scripts/PracticeDB_construction/ICD-11_6_8.csv", index=False)
+dfBoth.to_csv("/home/school/masters/Scripts/PracticeDB_construction/ICD-11_6_8.csv", index=False)"""
+
+# extracting a list of all the disorders in the ICD-11_6_8.csv removing the 4th level .something codes and saving it to ICD-11NoPointLevel
+path = "PracticeDB_construction/ICD-11_6_8.csv"
+df = pd.read_csv(path)
+dfNoPointLevel = pd.DataFrame(columns=df.columns)
+
+mask = ~(df['Code'].astype(str).str.contains(r'\.')) | df['Code'].isna()
+dfNoPointLevel = df[mask]
+dfNoPointLevel.to_csv("/home/school/masters/Scripts/PracticeDB_construction/ICD-11NoPointLevel.csv", index=False)
+
+
