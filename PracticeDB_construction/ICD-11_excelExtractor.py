b8499e43470c5808ee9b5f01e16585fa3e468f73
velma the vivobook <u21551228@tuks.co.za>
2026-03-31 12:49:49 +0200

    I extracted a csv (DTNPDmatchedICD11.csv) with the ICD-11 codes that match the DTNPD names in UniqueNeurologicalDiseases_DTNPD.csv but it doesnt look like it is complete. Ether the fuzzywuzzy logic, the Unique diseases or the fact that it just cant pull enough from the ICD-11_6_8 thing

--- PracticeDB_construction/ICD-11_excelExtractor.py
+++ PracticeDB_construction/ICD-11_excelExtractor.py
@@ -12,12 +12,24 @@ dfBoth = pd.concat([dfPsychoological, dfNeurological], ignore_index=True)
 dfBoth.to_csv("/home/school/masters/Scripts/PracticeDB_construction/ICD-11_6_8.csv", index=False)"""
 
 # extracting a list of all the disorders in the ICD-11_6_8.csv removing the 4th level .something codes and saving it to ICD-11NoPointLevel
-path = "PracticeDB_construction/ICD-11_6_8.csv"
+"""path = "PracticeDB_construction/ICD-11_6_8.csv"
 df = pd.read_csv(path)
 dfNoPointLevel = pd.DataFrame(columns=df.columns)
 
 mask = ~(df['Code'].astype(str).str.contains(r'\.')) | df['Code'].isna()
 dfNoPointLevel = df[mask]
-dfNoPointLevel.to_csv("/home/school/masters/Scripts/PracticeDB_construction/ICD-11NoPointLevel.csv", index=False)
+dfNoPointLevel.to_csv("/home/school/masters/Scripts/PracticeDB_construction/ICD-11NoPointLevel.csv", index=False)"""
 
+#extracting a csv (DTNPDmatchedICD11.csv) with the ICD-11 codes that match the DTNPD names in UniqueNeurologicalDiseases_DTNPD.csv
+from fuzzywuzzy import fuzz
+with open("/home/school/masters/Scripts/PracticeDB_construction/UniqueNeurologicalDiseases_DTNPD.txt", 'r') as file:
+    DTNPD_disease_names = [line.strip() for line in file]
 
+path = "PracticeDB_construction/ICD-11NoPointLevel.csv"
+df = pd.read_csv(path)
+
+dfMatched = pd.DataFrame(columns=df.columns)
+
+mask = df['Title'].fillna('').apply(lambda x: any(fuzz.partial_ratio(x, name) > 80 for name in DTNPD_disease_names))
+dfMatched = df[mask]
+dfMatched.to_csv("/home/school/masters/Scripts/PracticeDB_construction/DTNPDmatchedICD11.csv", index=False)
