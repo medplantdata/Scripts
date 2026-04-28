88fb65250f37a7346f0128a3e1b56e6276a5af87
velma the vivobook <u21551228@tuks.co.za>
2026-04-28 13:24:07 +0200

    Added a csv in the format for the plant family table for the database. Name, ID and then a FK id for the order ID

--- APG_IV/data-apg4-master/extracting_APG4_fot_DB.py
+++ APG_IV/data-apg4-master/extracting_APG4_fot_DB.py
@@ -1,19 +1,47 @@
 import pandas as pd
 
-apgDF = pd.read_csv('/home/school/masters/Scripts/APG_IV/data-apg4-master/apg4_data.csv')
+#////////////////////////////////////////////////this bit extracts the orders and gives them an id, then saves to a csv file.//////////////////////////////////////////////////////
+"""apgDF = pd.read_csv('/home/school/masters/Scripts/APG_IV/data-apg4-master/apg4_data.csv')
 apgDF.columns = apgDF.columns.str.strip()  
 print(apgDF.columns.to_list())
 
 
-ordersDF = pd.DataFrame(columns=['id', 'order'])
+ordersDF = pd.DataFrame(columns=['order_id', 'order'])
 orders = []
-count = 0
+order_count = 0
 
 for name, taxonRank in apgDF[['Scientific Name','Taxon Rank']].values:
     if taxonRank == 'order':
-        count += 1
-        orders.append([count, name])
+        order_count += 1
+        orders.append([order_count, name])
 
-ordersDF = pd.DataFrame(orders, columns=['id', 'order'])
-ordersDF.to_csv('orders.csv', index=False)
+ordersDF = pd.DataFrame(orders, columns=['order_id', 'order'])
+ordersDF.to_csv('/home/school/masters/Scripts/APG_IV/data-apg4-master/orders.csv', index=False)
+"""
+#////////////////////////////////////////////////this bit extracts the families and gives them an id, and a FK to the order then saves to a csv file
+apgDF = pd.read_csv('/home/school/masters/Scripts/APG_IV/data-apg4-master/apg4_data.csv')
+apgDF.columns = apgDF.columns.str.strip()
+
+ordersDF = pd.DataFrame(columns=['order_id', 'order'])
+familiesDF = pd.DataFrame(columns=['family_id', 'family', 'order_id'])
+orders = []
+families = []
+order_count = 0
+family_count = 0
+
+for name, taxonRank in apgDF[['Scientific Name','Taxon Rank']].values:
+    if taxonRank == 'order':
+        order_count += 1
+        orders.append([order_count, name])
+    elif taxonRank == 'family':
+        family_count += 1
+        families.append([family_count, name, order_count])
+
+familiesDF = pd.DataFrame(families, columns=['family_id', 'family', 'order_id'])
+familiesDF.to_csv('/home/school/masters/Scripts/APG_IV/data-apg4-master/families.csv', index=False)
+
+
+
+
+
 
