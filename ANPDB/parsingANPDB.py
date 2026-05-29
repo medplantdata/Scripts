6fd11d9eb9d70da2da0a9e93352eeaa6f369d69c
velma the vivobook <u21551228@tuks.co.za>
2026-05-29 22:22:05 +0200

    ran the script for a long time and discovered that I can be more efficient - check labbook 29 mmay

--- ANPDB/parsingANPDB.py
+++ ANPDB/parsingANPDB.py
@@ -3,7 +3,7 @@ from rdkit.Chem import PandasTools
 import pandas as pd
 import requests
 import time
-
+"""
 # converting sdf to csv and getting rid of unnecessary columns
 sdfFile = '/home/school/masters/Scripts/ANPDB/anpdb-05-2026.sdf'
 
@@ -49,4 +49,62 @@ cleaned_df['is_plant'] = is_plant
 plants_df = cleaned_df[cleaned_df['is_plant']].drop(columns=['is_plant'])
 plants_df.to_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv', index=False)
 print(errors)
-    
+"""
+
+# getting GBIF accepted name and ids for all the plants so I can make a junction table
+URL = "https://api.gbif.org/v1/species/match"
+df = pd.read_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv')
+
+def progress_bar(count, total):
+    bar_length = 40
+    filled_length = int(bar_length * count // total)
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+    print(f'\rProgress: |{bar}| {count}/{total} ({(count/total)*100:.2f}%)', end='')
+
+organismCount = 0
+for organisms in df['organisms']:
+    organisms = organisms.split('|')
+    for organism in organisms:
+        organismCount+=1
+
+print(f"Total organisms to process: {organismCount}")
+
+errors = []
+gbif_ids = []
+gbif_accepted_names = []
+count = 0
+for organisms in df['organisms']:
+    organisms = organisms.split('|')
+    idList = ''
+    nameList = ''
+    for organism in organisms:
+        count += 1
+        organism = organism.strip()
+        params = {"name": organism}
+        try:
+            time.sleep(0.5)
+            r = requests.get(URL, params=params, timeout=10)
+            r.raise_for_status()
+            data = r.json()
+            gbif_accepted_name = data.get("scientificName", "NA")
+            gbif_accepted_usagekey = data.get("acceptedUsageKey", "NA")
+            gbif_usagekey = data.get("usageKey", "NA")
+            print(f"{count} - Processed: {organism} - GBIF ID: {gbif_usagekey} - Accepted GBIF ID: {gbif_accepted_usagekey} - Accepted Name: {gbif_accepted_name}")
+            print(progress_bar(count, organismCount))
+            if gbif_accepted_usagekey is not None:
+                idList += f"{gbif_accepted_usagekey}|"
+                nameList += f"{gbif_accepted_name}|"
+            else:
+                idList += f"{gbif_usagekey}|"
+                nameList += f"{organism}|"
+        except requests.exceptions.RequestException as e:
+            print(f"Error for {count} - {organism}: {e}")
+            error = f"Error for {organism}: {e}"
+            errors.append(error)
+    gbif_ids.append(idList[:-1])
+    gbif_accepted_names.append(nameList[:-1])
+
+df['gbif_accepted_names'] = gbif_accepted_names
+df['gbif_ids'] = gbif_ids
+df.to_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants_gbifAccepted.csv', index=False)
+print(errors)
