0b25a8c2452875b2b40c5d0a84a5354c26173a27
velma the vivobook <u21551228@tuks.co.za>
2026-06-02 13:46:46 +0200

    New script to try sort out the ANPDB stuff and get accepted names and map it to the families and make sure it doesn't have any non plants just havent run yet

--- ANPDB/parsingANPDB.py
+++ ANPDB/parsingANPDB.py
@@ -50,7 +50,7 @@ plants_df = cleaned_df[cleaned_df['is_plant']].drop(columns=['is_plant'])
 plants_df.to_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv', index=False)
 print(errors)
 """
-
+"""
 # getting GBIF accepted name and ids for all the plants so I can make a junction table
 URL = "https://api.gbif.org/v1/species/match"
 df = pd.read_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv')
@@ -108,3 +108,67 @@ df['gbif_accepted_names'] = gbif_accepted_names
 df['gbif_ids'] = gbif_ids
 df.to_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants_gbifAccepted.csv', index=False)
 print(errors)
+"""
+
+# the above script takes comicaly long to run so I need to do it different and find out how to do bulk download or sum
+# seeing how many plants there are there
+"""df = pd.read_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv')
+organismCount = 0
+for organisms in df['organisms']:
+    organisms = organisms.split('|')
+    for organism in organisms:
+        organismCount+=1
+print(f"Total organisms to process: {organismCount}")
+
+orgSet = set()
+for organisms in df['organisms']:
+    organisms = organisms.split('|')
+    for organism in organisms:
+        orgSet.add(organism.strip())
+print(f"Total unique organisms to process: {len(orgSet)}")"""
+
+#creating a unique list with the name that gets the accepted name the family and the kingdom so i can make it plants only
+# and map it to db later
+
+df = pd.read_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv')
+organisms = df['organisms'].str.split('|').explode().str.strip().unique()
+
+URL = "https://api.gbif.org/v1/species/match"
+results = []
+
+def progress_bar(count, total):
+    bar_length = 40
+    filled_length = int(bar_length * count // total)
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+    print(f'\rProgress: |{bar}| {count}/{total} ({(count/total)*100:.2f}%)', end='')
+
+count = 0
+error = []
+entry = []
+for organism in organisms:
+    params = {"name": organism}
+    count += 1
+    try:
+        time.sleep(0.2)
+        r = requests.get(URL, params=params, timeout=10)
+        r.raise_for_status()
+        data = r.json()
+        gbif_accepted_name = data.get("scientificName", "NA")
+        gbif_accepted_usagekey = data.get("acceptedUsageKey", "NA")
+        gbif_usagekey = data.get("usageKey", "NA")
+        kingdom = data.get("kingdom", "NA")
+        print(f"Processed: {organism}")
+        print(progress_bar(count, len(organisms)))
+        if kingdom == "Plantae":
+            entry.append((organism, gbif_usagekey, gbif_accepted_usagekey, gbif_accepted_name, kingdom))
+        else:
+            print(f"{organism} is not a plant, skipping.")
+            error.append(f"{organism} is not a plant, skipping.")
+    except requests.exceptions.RequestException as e:
+        print(f"Error for {organism}: {e}")
+        error.append(f"Error for {organism}: {e}")
+
+df_plants = pd.DataFrame(entry, columns=['original_name', 'gbif_id', 'gbif_accepted_id', 'gbif_accepted_name', 'kingdom'])
+df_plants.to_csv('/home/school/masters/Scripts/ANPDB/unique_plants_with_correct_Names.csv', index=False)
+        
+print(error)
