571a8c6be85e07702941f836c35bc23d4df901e9
velma the vivobook <u21551228@tuks.co.za>
2026-06-24 12:01:01 +0200

    figured how to add the compounds and their related np classification now just have to actually do it

--- ANPDB/np_classifier_compound_junction_tables.py
+++ ANPDB/np_classifier_compound_junction_tables.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import requests
-# creating a junction table that will link the compound ID to a npclassifier class id
+# creating a junction table that will link the compound ID to a npclassifier class id, superclass id and then pathway id
 compoundsdf = pd.read_csv('/home/school/masters/Scripts/ANPDB/anpdb_plants.csv')
 
 classdf = pd.read_csv('/home/school/masters/Scripts/NpClassifier_ClassyFire/NpClassifierClassesDB.csv')
@@ -9,17 +9,23 @@ entry =[]
 
 url = 'https://npclassifier.gnps2.org/classify?smiles=<smiles string>'
 
-
-for coconut_id, classname in compoundsdf[['identifier', 'np_classifier_class']].values:
-    if classname == 'nan':
-        r = requests.get(url.replace('<smiles string>', coconut_id), timeout=10)
+error =[]
+for coconut_id, smiles, classname in compoundsdf[['identifier','canonical_smiles','np_classifier_class']].values:
+    if pd.isna(classname):
+        r = requests.get(url.replace('<smiles string>', smiles), timeout=10)
         r.raise_for_status()
         data = r.json()
         try:
             class_id = classdict[data['class']]
             entry.append((coconut_id, class_id, data['class']))
+            print(f"Processed: {coconut_id} it is a {data['class']}")
         except KeyError:
             entry.append((coconut_id, None, None))
+            error.append(f"Error for {coconut_id}")
+    else:
+        entry.append((coconut_id, classdict[classname], classname))
 junction1 = pd.DataFrame(entry, columns=['compound_id', 'class_id', 'class'])
+
+print(error)
 
 print(junction1.head())
