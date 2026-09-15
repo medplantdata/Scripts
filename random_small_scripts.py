62dcd5c3f969b3e3e1b15a5d6ebb100bbd61d313
velma the vivobook <u21551228@tuks.co.za>
2026-09-15 14:16:07 +0200

    Got the coconut things cleaned more or less on wonko

--- random_small_scripts.py
+++ random_small_scripts.py
@@ -1,8 +1,14 @@
-columns = 'count,gbif_taxon_key,family,scientific_name,scientific_name_authorship,taxonomic_status,acceptednameusageid,parentnameusageid,originalnameusageid,namepublishedin,scientificnameid,dynamicproperties,references'
+"""columns = 'count,gbif_taxon_key,family,scientific_name,scientific_name_authorship,taxonomic_status,acceptednameusageid,parentnameusageid,originalnameusageid,namepublishedin,scientificnameid,dynamicproperties,references'
 col = columns.split(',')
 out = ''
 
 for co in col:
     out = f'{out}, "{co.strip()}"'
 
-print(out)
+print(out)"""
+
+s1 = 'InChI=1S/C7H9N5O2/c1-12-3-4(10-7(12)13)9-6(8)11-5(3)14-2/h1-2H3,(H3,8,9,10,11,13)'
+s2 = 'InChI=1S/C7H9N5O2/c1-12-3-4(10-7(12)13)9-6(8)11-5(3)14-2/h1-2H3,(H3,8,9,10,11,13)'
+
+if s1 == s2:
+    print('Equal')
